128830e4cc95bf26efdfabfd3cbd4b9752ef228a
jhonned01 <69687137+jhonned01@users.noreply.github.com>
2024-05-11 10:14:52 -0500

    Update layout.js
    Deploy

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Branding Emocion",
-  description: "¡Optimiza tu presencia en línea con nuestra empresa especializada en diseño web y marketing digital! Brindamos soluciones personalizadas para empresas que desean destacarse en la era digital. ¡Contáctanos hoy mismo!"",
+  description: "¡Optimiza tu presencia en línea con nuestra empresa especializada en diseño web y marketing digital! Brindamos soluciones personalizadas para empresas que desean destacarse en la era digital. ¡Contáctanos hoy mismo!",
 };
 
 export default function RootLayout({ children }) {
