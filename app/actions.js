744bbc575c3b482ae71309ab43c1c029f99a68a5
jhonned01 <jhonned01@gmail.com>
2024-05-11 10:09:12 -0500

    Listo

--- app/actions.js
+++ app/actions.js
@@ -1,7 +1,7 @@
 "use server";
 import nodemailer from "nodemailer";
 
-export async function SendMail(FormData) {
+export async function SendMail(formData) {
   try {
     const {
       NombreCompleto,
@@ -11,35 +11,47 @@ export async function SendMail(FormData) {
       Correo,
       DNI,
       DominioSugerido,
-    } = await FormData;
-    console.log(FormData);
+    } = Object.fromEntries(formData);
 
     const transporter = nodemailer.createTransport({
       host: "smtp.zoho.com",
       port: 465,
       secure: true,
       auth: {
-        user: "soporte@brandingemocion.com",
-        pass: "2o#tsUem",
+        user: "notificacion@brandingemocion.com",
+        pass: "n7t!gqyQ",
       },
     });
 
     const mensaje = {
-      from: "soporte@brandingemocion.com",
+      from: "notificacion@brandingemocion.com",
       to: "fanaticos@emocion.pe,jhonned01@gmail.com",
       subject: `🎉🥳 ¡Solicitud de contacto: ${NombreEmpresa || ""}! 🥳🎉`,
       html: `
           <div style="max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
             <div style="background-color: #101c71; padding: 20px; color: #ffffff; border-radius: 5px 5px 0 0;">
-              <h1 style="margin-top: 0; font-size: 24px;">Jk Inmobiliaria</h1>
+              <h1 style="margin-top: 0; font-size: 24px;">Branding Emocion</h1>
             </div>
             <div style="padding: 20px;">
               <p style="margin-bottom: 15px;">Hemos recibido una solicitud de contacto de un nuevo usuario que se contactó a través de la página principal.</p>
               <p style="margin-bottom: 15px;">A continuación, encontrarás los detalles proporcionados:</p>
               <ul style="list-style: none; padding: 0; margin-bottom: 15px;">
-              <li style="margin-bottom: 10px;"><strong>Nombre Completo:</strong> ${NombreCompleto}</li>
-               <li style="margin-bottom: 10px;"><strong>Correo electrónico:</strong> <a href="mailto:${Correo}" style="color: #101c71; text-decoration: none;">${Correo}</a></li>
-                 <li style="margin-bottom: 10px;"><strong>DNI:</strong> ${DNI}</li>
+              <li style="margin-bottom: 10px;"><strong>Nombre Completo:</strong> ${
+                NombreCompleto || ""
+              }</li>
+              <li style="margin-bottom: 10px;"><strong>Nombre Empresa:</strong> ${
+                NombreEmpresa || ""
+              }</li>
+              <li style="margin-bottom: 10px;"><strong>Celular:</strong> ${
+                CelularCountry || ""
+              } ${Celular || 0}</li>
+              <li style="margin-bottom: 10px;"><strong>Dominio Sugerido:</strong> ${DominioSugerido}</li>
+               <li style="margin-bottom: 10px;"><strong>Correo electrónico:</strong> <a href="mailto:${Correo}" style="color: #101c71; text-decoration: none;">${
+        Correo || ""
+      }</a></li>
+                 <li style="margin-bottom: 10px;"><strong>DNI:</strong> ${
+                   DNI || ""
+                 }</li>
               </ul>
               <p style="margin-bottom: 0;">Por favor, ponte en contacto con el usuario lo antes posible para atender su solicitud.</p>
               <p style="margin-bottom: 0;">¡Gracias!</p>
